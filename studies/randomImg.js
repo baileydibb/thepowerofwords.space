3d802b2a55a245041d06b47d87b103ae09cfe13a
baileydibb <51273222+baileydibb@users.noreply.github.com>
2020-01-06 08:07:16 +0000

    Add files via upload

--- studies/randomImg.js
+++ studies/randomImg.js
@@ -1,11 +1,11 @@
-window.onload =
-function generate_bkImage()
+window.onload = function generate_bkImage()
 {
   var randomImage = Math.round(Math.random() * (7) + 1);
   src = "barbara-kruger/image/my-work/" + randomImage + ".jpg";
 
   var image = document.getElementById("bkImage");
   image.setAttribute("src", src);
+  image.onload = function(){calculate_aspect(image);}
 
   generate_barsImage();
 }
@@ -17,6 +17,7 @@ function generate_barsImage()
 
   var image = document.getElementById("barsImage");
   image.setAttribute("src", src);
+  image.onload = function(){calculate_aspect(image);}
 
   generate_dcImage();
 }
@@ -28,6 +29,7 @@ function generate_dcImage()
 
   var image = document.getElementById("dcImage");
   image.setAttribute("src", src);
+  image.onload = function(){calculate_aspect(image);}
 
   generate_krImage();
 }
@@ -39,6 +41,19 @@ function generate_krImage()
 
   var image = document.getElementById("krImage");
   image.setAttribute("src", src);
+  image.onload = function(){calculate_aspect(image);}
+
+  generate_lwImage();
+}
+
+function generate_lwImage()
+{
+  var randomImage = Math.round(Math.random() * (8) + 1);
+  src = "lawrence-weiner/image/my-work/" + randomImage + ".png";
+
+  var image = document.getElementById("lwImage");
+  image.setAttribute("src", src);
+  image.onload = function(){calculate_aspect(image);}
 
   generate_logosImage();
 }
@@ -50,6 +65,19 @@ function generate_logosImage()
 
   var image = document.getElementById("logosImage");
   image.setAttribute("src", src);
+  image.onload = function(){calculate_aspect(image);}
+
+  generate_taImage();
+}
+
+function generate_taImage()
+{
+  var randomImage = Math.round(Math.random() * (6) + 1);
+  src = "tauba-auerbach/image/my-work/" + randomImage + ".jpg";
+
+  var image = document.getElementById("taImage");
+  image.setAttribute("src", src);
+  image.onload = function(){calculate_aspect(image);}
 
   generate_typoImage();
 }
@@ -61,4 +89,30 @@ function generate_typoImage()
 
   var image = document.getElementById("typoImage");
   image.setAttribute("src", src);
+  image.onload = function(){calculate_aspect(image);}
+
+  generate_46spImage()
+}
+
+function generate_46spImage()
+{
+  var randomImage = Math.round(Math.random() * (45) + 1);
+  src = "46-summer-pictures/image/" + randomImage + ".jpg";
+
+  var image = document.getElementById("46spImage");
+  image.setAttribute("src", src);
+  image.onload = function(){calculate_aspect(image);}
+}
+
+function calculate_aspect(image)
+{
+  var container = document.getElementsByClassName("preview-img");
+  container = container[0];
+  var height = image.height;
+  var width = image.width;
+  if (width > height)
+  {
+    var padding = (container.clientHeight - height) / 2;
+    image.style.padding = padding + "px 0px";
+  }
 }
